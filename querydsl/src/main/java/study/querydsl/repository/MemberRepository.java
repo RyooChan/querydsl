556bb6d2ef002ae0c71adc104ae77edfc01230cc
RyooChan <fbcks97@naver.com>
2022-03-20 03:01:39 +0900

    QuerydslPredicateExecutor 적용
    * 이는 여러 한계가 있다.
         * 1. 조인X(묵시적 조인은 가능하지만 left join이 불가능하다.)
         * 2. 클라이언트가 querydsl에 의존하게 된다.
         * 이런 단점으로 인해 실무에서는 이를 사용하지 않는다.

--- querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.List;
  * 사용자 정의 리포지토리
  * 를 사용해야 한다.
  */
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
     // select m from Member m where m.username = ?
     List<Member> findByUsername(String username);
 }
--- querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -98,5 +99,39 @@ public class MemberRepositoryTest {
 
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+    }
+
+    /**
+     * QuerydslPredicateExecutor사용방법
+     * 이는 여러 한계가 있다.
+     * 1. 조인X(묵시적 조인은 가능하지만 left join이 불가능하다.)
+     * 2. 클라이언트가 querydsl에 의존하게 된다.
+     * 이런 단점으로 인해 실무에서는 이를 사용하지 않는다.
+     * @throws Exception
+     */
+    @Test
+    public void querydslPredicateExecutorTest() throws Exception {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(10, 40)
+                .and(member.username.eq("member1"))
+        );
+        for (Member findMember : result) {
+            System.out.println("member = " + findMember);
+        }
     }
 }
